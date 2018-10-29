a0031553c9c8f329daf20b7ab5955cd5a76c61b5
Charles Garner <charles.garner@ada.ac.uk>
2018-10-29 18:00:19 +0000

    Implemented email array and addEmail function

--- models/person.js
+++ models/person.js
@@ -1,9 +1,16 @@
 // Your Person class goes here!
 class Person {
   constructor(firstName, surname, dob) {
-    this.firstName = firstName
+    this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
     this.surname = surname
     this.dob = dob
+    this.emails = []
+  }
+  fullname() {
+    return this.firstName + ' ' + this.surname
+  }
+  addEmail(email) {
+    this.emails.push(email)
   }
 }
 
--- test/person.test.js
+++ test/person.test.js
@@ -11,5 +11,26 @@ describe('Person', function() {
     expect(person.firstName).to.equal('Joe')
     expect(person.surname).to.equal('Bloggs')
     expect(person.dob).to.equal('1 Jan 1990')
+    expect(person.emails).to.deep.equal([])
+  })
+
+  it('should capitialise firstname', function() {
+    var person = new Person('joe', 'Bloggs', '1 Jan 1990')
+
+    expect(person.firstName).to.equal('Joe')
+  })
+
+  it('should return a full name', function() {
+    var person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+
+    expect(person.fullname()).to.equal('Joe Bloggs')
+  })
+
+  it('should accept new emails and add them to the array', function() {
+    var person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+
+    person.addEmail('joe@example.com')
+    person.addEmail('joe.bloggs@work.com')
+    expect(person.emails).to.deep.equal(['joe@example.com','joe.bloggs@work.com'])
   })
 })
