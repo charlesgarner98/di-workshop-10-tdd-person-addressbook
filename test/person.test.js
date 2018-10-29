1d11129ab0d8a3bf7ab2bc387dcfc2624f3d3b9a
Charles Garner <charles.garner@ada.ac.uk>
2018-10-29 12:34:53 +0000

    Added constructor

--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,15 @@
 const chai = require('chai')
 const expect = chai.expect
 
+var Person = require('../models/Person.js')
+
 // Your Person test goes here!
+describe('Person', function() {
+  it('should initialise properly', function() {
+    var person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+
+    expect(person.firstName).to.equal('Joe')
+    expect(person.surname).to.equal('Bloggs')
+    expect(person.dob).to.equal('1 Jan 1990')
+  })
+})
